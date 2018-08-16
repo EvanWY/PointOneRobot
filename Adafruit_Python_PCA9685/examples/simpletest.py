e95bd0296a3b1504a6cfb33273df17224cfffa2b
My PI <pi@pi.com>
2018-08-16 09:14:15 +0000

    ädd something

---
+++ Adafruit_Python_PCA9685/examples/simpletest.py
@@ -0,0 +1,53 @@
+# Simple demo of of the PCA9685 PWM servo/LED controller library.
+# This will move channel 0 from min to max position repeatedly.
+# Author: Tony DiCola
+# License: Public Domain
+from __future__ import division
+import time
+import math
+from tqdm import tqdm
+
+# Import the PCA9685 module.
+import Adafruit_PCA9685
+
+
+# Uncomment to enable debug output.
+#import logging
+#logging.basicConfig(level=logging.DEBUG)
+
+# Initialise the PCA9685 using the default address (0x40).
+pwm = Adafruit_PCA9685.PCA9685()
+# Alternatively specify a different address and/or bus:
+#pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
+
+FREQUENCY = 120
+
+# Set frequency to 60hz, good for servos.
+pwm.set_pwm_freq(FREQUENCY)
+
+# Helper function to make setting a servo pulse width simpler.
+def pulse(channel, pulse_us):
+    pulse_length = 1000000    # 1,000,000 us per second
+    pulse_length //= FREQUENCY
+    pulse_length //= 4096     # 12 bits of resolution
+    pulse_us //= pulse_length
+    pwm.set_pwm(channel, 0, pulse_us)
+
+def steering(value):
+    maxpulse = 1780
+    minpulse = 1200
+    diff = maxpulse - minpulse
+    current_pulse = int((value + 1) * 0.5 * diff + minpulse)
+    pulse(0, current_pulse)
+
+pulse(1, 1500)
+pulse(0, 1500)
+
+if __name__=="__main__":
+    time.sleep(3)
+
+    while True:
+        steering(0.9*math.sin(time.time()*10))
+        time.sleep(0.001)
+
+
