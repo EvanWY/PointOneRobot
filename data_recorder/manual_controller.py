5804eb5d6e37665d4c6395ebb87dba5635a7a491
Yang <wangyang.evan@gmail.com>
2018-08-18 06:05:32 -0700

    control

--- data_recorder/manual_controller.py
+++ data_recorder/manual_controller.py
@@ -12,6 +12,7 @@ windowSurface = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
 
 windowSurface.fill(BLACK)    
 pub = rospy.Publisher('/control/steering', Float64, queue_size=10)
+pub_throttle = rospy.Publisher('/control/throttle', Float64, queue_size=10)
 rospy.init_node('manual_controller', anonymous=True)
 
 while True:
@@ -23,21 +24,38 @@ while True:
             if event.key == pygame.K_1:
                 pub.publish(-1)
             if event.key == pygame.K_2:
-                pub.publish(-0.75)
-            if event.key == pygame.K_3:
                 pub.publish(-0.5)
-            if event.key == pygame.K_4:
+            if event.key == pygame.K_3:
                 pub.publish(-0.25)
+            if event.key == pygame.K_4:
+                pub.publish(-0.125)
             if event.key == pygame.K_5:
                 pub.publish(0)
             if event.key == pygame.K_6:
-                pub.publish(0.25)
+                pub.publish(0.125)
             if event.key == pygame.K_7:
-                pub.publish(0.5)
+                pub.publish(0.25)
             if event.key == pygame.K_8:
-                pub.publish(0.75)
+                pub.publish(0.5)
             if event.key == pygame.K_9:
                 pub.publish(1)
+
+            if event.key == pygame.K_SPACE:
+                pub_throttle.publish(0)
+            if event.key == pygame.K_z:
+                pub_throttle.publish(0.015)
+            if event.key == pygame.K_x:
+                pub_throttle.publish(0.03)
+            if event.key == pygame.K_c:
+                pub_throttle.publish(0.06)
+            if event.key == pygame.K_v:
+                pub_throttle.publish(0.1)
+            if event.key == pygame.K_b:
+                pub_throttle.publish(0.18)
+            if event.key == pygame.K_n:
+                pub_throttle.publish(0.27)
+            if event.key == pygame.K_m:
+                pub_throttle.publish(0.38)
                 
 
 
